1bd2f21853d30a734ba610f95cf6a9b46586c915
Kunal Jethwa <kunal.je@customer-acquisition.co>
2024-09-25 15:35:44 +0530

    Changes

--- EP14/netflixgpt-clone/Client/src/Components/MovieList/MoiveList.js
+++ EP14/netflixgpt-clone/Client/src/Components/MovieList/MoiveList.js
@@ -5,7 +5,7 @@ import './MovieList.css';
 const MovieList = ({ title, allMovieData }) => {
   return (
     <div className="sm:mt-[-20rem] mt-0 sm:ml-12 sm:bg-white bg-black p-6 movie-card">
-      <h2 className="sm:text-2xl text-md font-bold text-white sm:mb-4 mb-2 mt-4">{title}</h2>
+      <h2 className="sm:text-2xl sm:relative z-10 text-md font-bold text-white sm:mb-4 mb-2 mt-4">{title}</h2>
       <div className="overflow-x-auto scroll-smooth no-scrollbar relative z-20">
         <div className="flex space-x-4">
           {allMovieData.map((movie,index) => (
