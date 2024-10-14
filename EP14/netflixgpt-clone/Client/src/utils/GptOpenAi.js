1bf86ff8a6bbea5893adbaaa92ca4872dbe3153a
Kunal Jethwa <kunal.je@customer-acquisition.co>
2024-10-14 19:08:51 +0530

    Changes

--- EP14/netflixgpt-clone/Client/src/utils/GptOpenAi.js
+++ EP14/netflixgpt-clone/Client/src/utils/GptOpenAi.js
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 
 const GptOpenAi = new OpenAI({
-    apiKey: REACT_APP_OPEN_AI_KEY,
+    apiKey: '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',
     dangerouslyAllowBrowser: true,
 });
 
